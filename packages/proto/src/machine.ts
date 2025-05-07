506cd669cb61d678a6a6fb514d1dc40595f8e599
Ryan Xu <ryanxu240@gmail.com>
2025-05-07 15:04:08 -0700

    finished lab 9

--- packages/proto/src/machine.ts
+++ packages/proto/src/machine.ts
@@ -1,34 +1,62 @@
 import {html, css, LitElement} from "lit";
-import {property} from "lit/decorators.js";
+import {property, state} from "lit/decorators.js";
 import reset from "./styles/reset.css.ts";
 
 export class MachineElement extends LitElement {
-    @property({attribute: "img-src"})
-    imgSrc?: string;
+    @property()
+    src?: string;
+  
+    @state()
+    subtitle = '';
 
-    override render() {
-        return html`
-            <h2> <slot name="Subtitle">Default Subtitle</slot> </h2>
-            
-            <ol>
-                <slot> 
-                    <li>Default List</li>
-                </slot>
-            </ol>
+    @state()
+    steps: string[] = [];
 
-            <img .src=${this.imgSrc} alt="Machine image"/>
-            
-            `
-        ;
-    }
+    @state() 
+    imageSrc = '';
 
     static styles = [
-        reset.styles,
-        css`
-            img {
-                max-width: 100%;
-                height: auto;
-            }
+            reset.styles,
+            css`
+                img {
+                    max-width: 100%;
+                    height: auto;
+                }
+    
+        `];
 
-    `];
+    connectedCallback() {
+        super.connectedCallback();
+        if (this.src) this.hydrate(this.src);
+    }
+
+    async hydrate(src: string) {
+        try {
+          const res = await fetch(src);
+          if (!res.ok) {
+            console.error(`Failed to fetch in Instructions.ts - hydrate()`);
+            return;
+          }
+          const data = (await res.json()) as {
+            imageSrc: string;
+            subtitle: string;
+            steps: string[];
+          };
+          this.imageSrc = data.imageSrc;
+          this.subtitle = data.subtitle;
+          this.steps = data.steps;
+        } catch (e) {
+          console.error('Error loading JSON:', e);
+        }
+      }
+
+      render() {
+        return html`
+          <img src="${this.imageSrc}" />
+          <h2 slot="Subtitle">${this.subtitle}</h2>
+          <ol>
+            ${this.steps.map((step) => html`<li>${step}</li>`)}
+          </ol>
+        `;
+      }
 }
